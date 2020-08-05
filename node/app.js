2f5c227685e0d8f8c821b53eb47da5e1a5d8c6b0
1944271287 <1944271287@qq.com>
2020-08-05 15:50:03 +0800

    提交node目录

---
+++ node/app.js
@@ -0,0 +1,24 @@
+// 引入express
+var express = require('express');
+var app = express();
+
+// // 引入cors，解决跨域的问题
+// const cors = require('cors')
+// app.use(cors())
+
+// 开启服务,监听8888端口
+app.listen(8888,(req,res)=>{
+    console.log('您的应用程序正在这里运行：http://localhost:8888');
+})
+
+// 引入学生信息
+const student =  require('./api/data/student.js')
+
+app.all('/ceshi',(req,res)=>{
+    var code = {
+        data: student,
+        status:'成功调用测试接口，get请求',
+        status_code: 200,
+    }
+    res.send(code);
+})
